ede39755bb5328cb17a930b932336e99291a8cd9
Pijus P <pijus.paulikas@gmail.com>
2023-03-10 19:11:54 +0200

    handle empty key

--- scrapyd/sqlite.py
+++ scrapyd/sqlite.py
@@ -9,71 +9,138 @@ except ImportError:
     from collections import MutableMapping
 
 
+# class JsonSqliteDict(MutableMapping):
+#     """SQLite-backed dictionary"""
+
+#     def __init__(self, database=None, table="dict"):
+#         database_host = "redisdocker"
+#         database_port = 6379
+#         # database_user = self.get_optional_config(config, 'redis_user')
+#         # database_pwd = self.get_optional_config(config, 'redis_pass')
+
+#         self.conn = redis.StrictRedis(
+#             host=database_host,
+#             port=int(database_port),
+#             charset="utf-8",
+#             decode_responses=True,
+#             # db=int(database_name),
+#             # password=database_pwd
+#         )
+
+#         self.queue = "scrapyd-redis.queue.{}.{}".format(database, table)
+
+#     def __getitem__(self, key):
+#         try:
+#             self.decode(self.conn.get(key))
+#         except:
+#             return None
+
+#     def __setitem__(self, key, value):
+#         self.conn.set(key, self.encode(value))
+
+#     def __delitem__(self, key):
+#         self.conn.delete(key)
+
+#     def __len__(self):
+#         return len(self.conn.keys("*"))
+
+#     def __iter__(self):
+#         for k in self.iterkeys():
+#             yield k
+
+#     def iterkeys(self):
+#         for k in self.conn.keys("*"):
+#             yield k
+
+#     def keys(self):
+#         return list(self.conn.keys("*"))
+
+#     def itervalues(self):
+#         for k in self.conn.keys("*"):
+#             yield self.decode(self.conn.get(k))
+
+#     def values(self):
+#         return list(self.itervalues())
+
+#     def iteritems(self):
+#         for k in self.conn.keys("*"):
+#             yield k, self.decode(self.conn.get(k))
+
+#     def items(self):
+#         return list(self.iteritems())
+
+#     def encode(self, obj):
+#         return json.dumps(obj)
+
+#     def decode(self, text):
+#         return json.loads(text)
+
+
 class JsonSqliteDict(MutableMapping):
     """SQLite-backed dictionary"""
 
     def __init__(self, database=None, table="dict"):
-        database_host = "redisdocker"
-        database_port = 6379
-        # database_user = self.get_optional_config(config, 'redis_user')
-        # database_pwd = self.get_optional_config(config, 'redis_pass')
-
-        self.conn = redis.StrictRedis(
-            host=database_host,
-            port=int(database_port),
-            charset="utf-8",
-            decode_responses=True,
-            # db=int(database_name),
-            # password=database_pwd
-        )
-
-        self.queue = "scrapyd-redis.queue.{}.{}".format(database, table)
+        self.database = database or ":memory:"
+        self.table = table
+        # about check_same_thread: http://twistedmatrix.com/trac/ticket/4040
+        self.conn = sqlite3.connect(self.database, check_same_thread=False)
+        q = "create table if not exists %s (key blob primary key, value blob)" % table
+        self.conn.execute(q)
 
     def __getitem__(self, key):
-        try:
-            self.decode(self.conn.get(key))
-        except:
-            return None
+        key = self.encode(key)
+        q = "select value from %s where key=?" % self.table
+        value = self.conn.execute(q, (key,)).fetchone()
+        if value:
+            return self.decode(value[0])
+        raise KeyError(key)
 
     def __setitem__(self, key, value):
-        self.conn.set(key, self.encode(value))
+        key, value = self.encode(key), self.encode(value)
+        q = "insert or replace into %s (key, value) values (?,?)" % self.table
+        self.conn.execute(q, (key, value))
+        self.conn.commit()
 
     def __delitem__(self, key):
-        self.conn.delete(key)
+        key = self.encode(key)
+        q = "delete from %s where key=?" % self.table
+        self.conn.execute(q, (key,))
+        self.conn.commit()
 
     def __len__(self):
-        return len(self.conn.keys("*"))
+        q = "select count(*) from %s" % self.table
+        return self.conn.execute(q).fetchone()[0]
 
     def __iter__(self):
         for k in self.iterkeys():
             yield k
 
     def iterkeys(self):
-        for k in self.conn.keys("*"):
-            yield k
+        q = "select key from %s" % self.table
+        return (self.decode(x[0]) for x in self.conn.execute(q))
 
     def keys(self):
-        return list(self.conn.keys("*"))
+        return list(self.iterkeys())
 
     def itervalues(self):
-        for k in self.conn.keys("*"):
-            yield self.decode(self.conn.get(k))
+        q = "select value from %s" % self.table
+        return (self.decode(x[0]) for x in self.conn.execute(q))
 
     def values(self):
         return list(self.itervalues())
 
     def iteritems(self):
-        for k in self.conn.keys("*"):
-            yield k, self.decode(self.conn.get(k))
+        q = "select key, value from %s" % self.table
+        return ((self.decode(x[0]), self.decode(x[1])) for x in self.conn.execute(q))
 
     def items(self):
         return list(self.iteritems())
 
     def encode(self, obj):
-        return json.dumps(obj)
+        return sqlite3.Binary(json.dumps(obj).encode("ascii"))
 
-    def decode(self, text):
-        return json.loads(text)
+    def decode(self, obj):
+        return json.loads(bytes(obj).decode("ascii"))
 
 
 class JsonSqlitePriorityQueue(object):
